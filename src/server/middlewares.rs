7bc88548434f56a956750fab041e28ae03a08926
Jannik Obermann <jannik.obermann@gmail.com>
2021-04-20 17:44:54 +0200

    Add tests for authenticate middleware

--- src/server/middlewares.rs
+++ src/server/middlewares.rs
@@ -17,3 +17,43 @@ pub fn authenticate(auth_key: String) -> impl Filter<Extract = (), Error = Rejec
         })
         .untuple_one()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_authenticate_success() {
+        warp::test::request()
+            .header("x-auth-key", "1234")
+            .filter(&authenticate("1234".to_string()))
+            .await
+            .unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_authenticate_error() {
+        warp::test::request()
+            .filter(&authenticate("1234".to_string()))
+            .await
+            .unwrap_err();
+
+        warp::test::request()
+            .header("x-auth-key", "xxxx")
+            .filter(&authenticate("1234".to_string()))
+            .await
+            .unwrap_err();
+
+        warp::test::request()
+            .header("x-auth-key", "1234 ")
+            .filter(&authenticate("1234".to_string()))
+            .await
+            .unwrap_err();
+
+        warp::test::request()
+            .header("x-auth-key", "ABC")
+            .filter(&authenticate("abc".to_string()))
+            .await
+            .unwrap_err();
+    }
+}
